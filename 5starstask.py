07bf09d9f7f89ca70055dd78f824f0f89c4d8c34
Macbook <Shania@PeanutButterJelly.local>
2018-09-19 15:02:48 +0700

    first commit

--- 5starstask.py
+++ 5starstask.py
@@ -12,10 +12,27 @@
 #for i in range (1 , A+1):
 #    print("*" * ((A+1) - i))
 
-#stars = input("Stars")
-#x = 1
-#stars = int(stars)
-#while x <= stars:
-#    print(("*") * stars)
-#    x = x + 1
 
+#A = int(input("Insert a number:"))
+#for i in range (1 , A+1):
+#    print("*" * ((A-1) + i))
+
+#x = 5
+#while x >=1:
+#    print((" " * (5-x)) , ("*" * x))
+#    x = x-1
+
+x= int(input("insert a nnumber:"))
+i=1
+j=x
+k=1
+while k<=x:
+    print(" "*j,"*"*i)
+    i=i+2
+    j=j-1
+    k=k+1
+
+n = 5
+stars = [('*' + '**'*i).center(2*n + 1) for i in range(n)]
+
+print('\n'.join(stars + stars[::-1][1:]))
